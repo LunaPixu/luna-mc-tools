c6aea2d7cda028ebaf1758ed4c04a336f7eb972e
LunaPixu <officialtrxd@gmail.com>
2023-06-23 17:33:48 +0100

    Reverted parts of fa script to pre-port state Ported site: "Oops! All Icons!"

--- plugins/font-awesome.ts
+++ plugins/font-awesome.ts
@@ -1,12 +1,16 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCommentsDollar, faHouse, faShieldHalved, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 config.autoAddCss = false;
 
-library.add(fas);
-library.add(fab);
+library.add(faHouse);
+library.add(faCommentsDollar);
+library.add(faShieldHalved);
+library.add(faTwitter);
+library.add(faGithub);
+library.add(faBars);
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon);
