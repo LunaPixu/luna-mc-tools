c3785c8a11197cd7529704d53fd51e8f9468c23a
LunaPixu <officialtrxd@gmail.com>
2023-07-04 17:02:06 +0100

    Implemented help dialog buttons

---
+++ components/helpButton.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const props = defineProps({
+    id: { type: String, required: true },
+});
+function showDialog(): void {
+    const dialog = document.getElementById(`${props.id}-helpdialog`) as HTMLDialogElement;
+    dialog.showModal();
+}
+function closeDialog(): void {
+    const dialog = document.getElementById(`${props.id}-helpdialog`) as HTMLDialogElement;
+    dialog.close();
+}
+</script>
+
+<template>
+    <button class="tooltip-button" type="button" @click="showDialog()">?</button>
+    <dialog :id="`${id}-helpdialog`">
+        <button class="tooltip-button" id="close-button" type="button" @click="closeDialog()">X</button>
+        <slot />
+    </dialog>
+</template>
+
+<style scoped>
+#close-button {
+    float: right;
+}
+</style>
