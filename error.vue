2b20fe6bb3553d105e37dfcd0c2d4e283dc313ea
LunaPixu <officialtrxd@gmail.com>
2023-08-01 15:20:46 +0100

    Updated error screen text

--- error.vue
+++ error.vue
@@ -1,6 +1,7 @@
 <template>
   <h2>An Error Has Occured</h2>
   <p>Error {{ error.statusCode }} {{ error.statusMessage ? `- ${error.statusMessage}` : "" }}</p>
+  <p>Please contact Luna Pixu if this error persists.</p>
   <button @click="handleError">Return to website</button>
 </template>
 
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
+const route = useRoute();
 </script>
 
 <template>
   <h2>Error 404: Not Found</h2>
+  <p>
+    The page "{{ route.path }}" does not exist.<br />
+    It may have been moved, removed, or is not present on this version of Luna's Minecraft Tools.
+  </p>
+  <p>Please double-check the URL. If you believe your URL is valid and shouldn't route to this 404 page, please contact Luna Pixu.</p>
 </template>
 
 <style scoped></style>
